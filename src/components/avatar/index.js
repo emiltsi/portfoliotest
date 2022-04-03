c3064152d45b09a56c1045b47954400eeaabb3d7
emiltsi <101522174+emiltsi@users.noreply.github.com>
2022-04-04 00:25:57 +0100

    Change back to pre-bounds

--- src/components/avatar/index.js
+++ src/components/avatar/index.js
@@ -3,8 +3,8 @@ import * as THREE from "three";
 import { Suspense, useEffect, useState } from "react";
 
 import { Canvas } from "@react-three/fiber";
+import { Bounds, useBounds, OrbitControls, ContactShadows, useGLTF } from '@react-three/drei'
 import Model from "./model";
-import { Bounds, useBounds,} from '@react-three/drei'
 
 export default function M() {
   const [position, setPosition] = useState([0, 0, 0]);
@@ -22,10 +22,11 @@ export default function M() {
   return (
     <div style={{ width: "100%", height: "100%" }}>
       <Canvas
-        linear
-        gl={{ toneMappingExposure = 1.5 }}
-        // Clamp window.devicePixelRatio between 1-2, this is the default on v8
-        dpr={[1, 2]}
+        onCreated={({ gl }) => {
+          gl.toneMapping = THREE.LinearToneMapping;
+          gl.toneMappingExposure = 1.5;
+        }}
+        dpr={window.devicePixelRatio}
         camera={{
           position: [0, 1, 5],
           fov: 50,
